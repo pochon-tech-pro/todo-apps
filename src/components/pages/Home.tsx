53fd8422eb4f408fe0067c0c638fa31ad213650b
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-01 17:31:56 +0900

    feat: デザイン調整

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,6 @@
 import { ChangeEvent, MouseEvent, useState, VFC } from 'react';
+import styled from 'styled-components';
+import { FormControl, List, TextField } from '@material-ui/core';
 
 type Task = { id: string; title: string };
 
@@ -14,27 +16,60 @@ export const Home: VFC = () => {
 
   const addTask = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setTasks([...tasks, {
-      id: Math.random().toString(32).substring(2),
-      title: input,
-    }]);
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random().toString(32).substring(2),
+        title: input,
+      },
+    ]);
   };
 
   return (
-    <>
-      <h1>Home</h1>
-      <form>
-        <input
+    <SRoot>
+      <h1>タスク管理</h1>
+      <FormControl>
+        <TextField
           value={input}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setInput(event.target.value)
           }
         />
-        <button onClick={addTask}>タスク追加</button>
-      </form>
-      {tasks.map((item) => {
-        return <div key={item.id}> {item.title}</div>;
-      })}
-    </>
+      </FormControl>
+      <SButton onClick={addTask}>タスク追加</SButton>
+      <List>
+        {tasks.map((item) => {
+          return <SItem key={item.id}> {item.title}</SItem>;
+        })}
+      </List>
+    </SRoot>
   );
 };
+
+const SRoot = styled.div`
+  text-align: center;
+  color: gray;
+  font-family: serif;
+`;
+
+const SButton = styled.button`
+  background-color: gray;
+  color: aliceblue;
+  border: none; // 枠線なくす
+  padding: 8px; // 中に余白をつける
+  margin: 0 0 0 10px;
+  border-radius: 8px; // 角を丸く
+  &:hover {
+    background-color: brown;
+    color: aliceblue;
+    font-weight: bolder;
+    cursor: pointer;
+  }
+`;
+
+const SItem = styled.div`
+  text-align: center;
+  color: black;
+  margin: 10px;
+  font-family: serif;
+`;
