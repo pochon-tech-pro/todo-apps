028cb1aae97c1bfefe223010a3b9605ed25eb1bc
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-01 15:55:21 +0900

    feat: Task

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,40 @@
-import { VFC } from 'react';
+import { ChangeEvent, MouseEvent, useState, VFC } from 'react';
+
+type Task = { id: string; title: string };
+
+const dummy: Task[] = [
+  { id: 'AAAA', title: 'Task1' },
+  { id: 'BBBB', title: 'Task2' },
+  { id: 'CCCC', title: 'Task3' },
+];
 
 export const Home: VFC = () => {
-  return <p>Home</p>;
+  const [tasks, setTasks] = useState<Task[]>(dummy);
+  const [input, setInput] = useState('');
+
+  const addTask = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setTasks([...tasks, {
+      id: Math.random().toString(32).substring(2),
+      title: input,
+    }]);
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <form>
+        <input
+          value={input}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setInput(event.target.value)
+          }
+        />
+        <button onClick={addTask}>タスク追加</button>
+      </form>
+      {tasks.map((item) => {
+        return <div key={item.id}> {item.title}</div>;
+      })}
+    </>
+  );
 };
