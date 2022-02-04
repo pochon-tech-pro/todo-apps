5981e23b207be446c4efbf89ee41ff489211b993
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-02-04 11:32:41 +0900

    fix: ApolloClient導入によりJestがコケていたので修正

--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Home } from '../components/pages/Home';
+import { MockedProvider } from '@apollo/client/testing';
 
 test('renders learn react link', () => {
-  render(<Home />);
+  render(
+    <MockedProvider>
+      <Home />
+    </MockedProvider>
+  );
   const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
